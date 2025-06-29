1ae1de80dd82c2a06c214e57585a1f7e469f17c6
Aslan049 <aslankorkmaz049@gmail.com>
2025-06-29 14:44:58 +0300

    first commit

---
+++ GHFollowers/GHFollowers/FavoriteListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by Aslan Korkmaz on 28.06.2025.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        
+    }
+
+}
---
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -0,0 +1,118 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Aslan Korkmaz on 28.06.2025.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    let logoImageView       = UIImageView()
+    let usernameTextField   = GFTextField()
+    let callToActionButton  = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    var isUserNameEntered: Bool { return !usernameTextField.text!.isEmpty }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextField()
+        configureCallToActionButton()
+        createDismissKeyboardTapGestureRecognizer()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.backgroundColor = .systemBackground
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    func createDismissKeyboardTapGestureRecognizer() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func alertForEmptyUsername() {
+        let alertController = UIAlertController(title: "Opps", message: "You must enter a username", preferredStyle: .alert)
+        let okAction        = UIAlertAction(title: "OK", style: .default)
+        let whyAction       = UIAlertAction(title: "Why", style: .default, handler: { _ in
+            if let url = URL(string: "https://github.com/") {
+                UIApplication.shared.open(url)
+            }
+        })
+        
+        alertController.addAction(okAction)
+        alertController.addAction(whyAction)
+        navigationController?.present(alertController, animated: true)
+    }
+    
+    @objc func pushFollowerListVC() {
+        guard isUserNameEntered else {
+            alertForEmptyUsername()
+            return
+        }
+        
+        let followerListVC      = FollowerListVC()
+        followerListVC.username = usernameTextField.text
+        followerListVC.title    = usernameTextField.text
+        navigationController?.pushViewController(followerListVC, animated: true)
+        
+        usernameTextField.text = ""
+    }
+    
+    
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo") ?? UIImage(named: "gh2")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        usernameTextField.delegate = self
+
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        callToActionButton.addTarget(self, action: #selector(pushFollowerListVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
+
+
+extension SearchVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pushFollowerListVC()
+        return true
+    }
+}
